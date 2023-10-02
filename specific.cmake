b204fa4e2592dd881aa8b569606dc6c843cace55
Wai-Shing Luk <luk036@gmail.com>
2023-10-02 10:42:38 +0800

    remove cppcoro::cppcoro

--- specific.cmake
+++ specific.cmake
@@ -13,18 +13,6 @@ CPMAddPackage(
 # SYSTEM INTERFACE ${Boost_INCLUDE_DIRS}) # Disable autolink target_compile_definitions(Boost::boost
 # INTERFACE BOOST_ALL_NO_LIB=1) endif()
 
-CPMAddPackage(
-  NAME cppcoro
-  GIT_TAG 2.0
-  GITHUB_REPOSITORY luk036/cppcoro
-)
-# print_target_properties(cppcoro)
-if(cppcoro_ADDED)
-  message(STATUS "Found cppcoro: ${cppcoro_SOURCE_DIR}")
-  add_library(cppcoro::cppcoro INTERFACE IMPORTED GLOBAL)
-  target_include_directories(cppcoro::cppcoro SYSTEM INTERFACE ${cppcoro_SOURCE_DIR}/include)
-endif(cppcoro_ADDED)
-
 if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   # using GCC
   add_compile_options(-fcoroutines)
