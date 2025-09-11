0103a908a5a89adc53842641f8bb254983a917a6
Wai-Shing Luk <luk036@gmail.com>
2025-09-11 07:52:09 +0000

    apply formating

--- test/source/test_parametric_raw.cpp
+++ test/source/test_parametric_raw.cpp
@@ -1,22 +1,21 @@
 #include <doctest/doctest.h>
+
 #include <cstdint>  // for uint32_t
 #include <digraphx/parametric.hpp>
 #include <list>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 using std::list;
 using std::pair;
-using std::vector;
 using std::unordered_map;
+using std::vector;
 
 TEST_CASE("Test Parametric Search (list of lists)") {
     list<pair<size_t, list<pair<size_t, int>>>> gra{
         {0, {{1, 5}, {2, 1}}}, {1, {{0, 1}, {2, 1}}}, {2, {{1, 1}, {0, 1}}}};
 
-    auto distance = [](double r, const auto& edge) {
-        return edge - r;
-    };
+    auto distance = [](double r, const auto& edge) { return edge - r; };
 
     auto zero_cancel = [](const auto& cycle) {
         double cost = 0;
@@ -37,9 +36,7 @@ TEST_CASE("Test Parametric Search (dict of list's)") {
         {0, {{1, 0}, {2, 1}}}, {1, {{0, 2}, {2, 3}}}, {2, {{1, 4}, {0, 5}}}};
     const vector<int> edge_cost{5, 1, 1, 1, 1, 1};
 
-    auto distance = [&edge_cost](double r, const auto& edge) {
-        return edge_cost.at(edge) - r;
-    };
+    auto distance = [&edge_cost](double r, const auto& edge) { return edge_cost.at(edge) - r; };
 
     auto zero_cancel = [&edge_cost](const auto& cycle) {
         double cost = 0;
