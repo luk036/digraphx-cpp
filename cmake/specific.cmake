6f5f897f923657f2dc319d7a30a00ddda300aa5c
Wai-Shing Luk <luk036@gmail.com>
2026-04-30 19:26:45 +0800

    upgrade fmt 12.1.0 and spdlog 1.17.0

--- cmake/specific.cmake
+++ cmake/specific.cmake
@@ -3,7 +3,7 @@ find_package(Threads REQUIRED)
 
 CPMAddPackage(
   NAME fmt
-  GIT_TAG 11.0.2
+  GIT_TAG 12.1.0
   GITHUB_REPOSITORY fmtlib/fmt
   OPTIONS "FMT_INSTALL YES" # create an installable target
 )
@@ -24,7 +24,7 @@ CPMAddPackage(
 
 CPMAddPackage(
   NAME spdlog
-  GIT_TAG v1.12.0
+  GIT_TAG v1.17.0
   GITHUB_REPOSITORY gabime/spdlog
   OPTIONS "SPDLOG_INSTALL YES" # create an installable target
 )
