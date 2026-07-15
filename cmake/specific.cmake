f424a4fee0bf1ef8d3b4f92ffdbce9d31fcd1715
Wai-Shing Luk <luk036@gmail.com>
2026-07-15 15:34:25 +0800

    release: bump v1.1.5→v1.1.6, update Py2Cpp v1.6.3 / MyWheel v1.1.5 tags, suppress C4324 on MSVC

--- cmake/specific.cmake
+++ cmake/specific.cmake
@@ -10,14 +10,14 @@ CPMAddPackage(
 
 CPMAddPackage(
   NAME Py2Cpp
-  GIT_TAG 1.6.0
+  GIT_TAG v1.6.3
   GITHUB_REPOSITORY luk036/py2cpp
   OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
 CPMAddPackage(
   NAME MyWheel
-  GIT_TAG 1.1.3
+  GIT_TAG v1.1.5
   GITHUB_REPOSITORY luk036/mywheel-cpp
   OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
